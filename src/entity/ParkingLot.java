f4ddc38564d0d624d07f4e51203078a771a9be4d
gourisuraj <gouri.suraj@mail.utoronto.ca>
2024-07-04 19:12:46 -0400

    implemented draft getReview method

--- src/entity/ParkingLot.java
+++ src/entity/ParkingLot.java
@@ -2,23 +2,39 @@ package entity;
 
 import Review.java;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class ParkingLot {
     private final String streetAddress;
     private final String linkToWebsite;
-    private final Array<float> latitudeLongitude;
-    private final Array<Review> reviews;
+    private final ArrayList<Float> latitudeLongitude;
     private final HashMap<String, String> timesToRates;
+    public ArrayList<Integer> easeOfFindingReviews;
+    public ArrayList<Integer> easeOfEntryReviews;
 
     /**
     * Requires: xxxxx
     * @param streetAddress
     */
     ParkingLot(String streetAddress) {
-        * TODO IMPLEMENT
+        this.streetAddress = streetAddress;
 
     }
 
-
+    public <T> getEntryReview() {
+        if (easeOfEntryReviews.isEmpty()) {
+            return "null";
+        }
+        else {
+            int count = easeOfEntryReviews.size();
+            int sum = 0;
+            for (int i = 0; i < count; i++) {
+                sum += easeOfEntryReviews.get(i);
+            }
+            int avg = sum / count;
+            return avg;
+    }
 
     public String getAddress() {return streetAddress; }
 
