85e9315cf5e4c2221371c36f725a94b6d44f4e76
gourisuraj <gouri.suraj@mail.utoronto.ca>
2024-07-04 16:16:48 -0400

    edited ParkingSpot.java to begin construction of the entity

---
+++ src/entity/ParkingSpot.java
@@ -0,0 +1,29 @@
+package entity;
+
+import Review;
+
+class ParkingSpot {
+    private final String streetAddress;
+    private final String linkToWebsite;
+    private final Array<float> latitudeLongitude;
+    private final Array<Review> reviews;
+    private final HashMap<String, String> timesToRates;
+
+    /**
+    * Requires: xxxxx
+    * @param streetAddress
+    */
+    ParkingSpot(String streetAddress) {
+        * TODO IMPLEMENT
+
+    }
+
+    public String getAddress() {return streetAddress; }
+
+    public String getWebsiteLink() {return linkToWebsite; }
+
+    public Array<Review> getReviews() {return reviews; }
+
+    public HashMap<String, String> getRates() {return timesToRates; }
+
+}
