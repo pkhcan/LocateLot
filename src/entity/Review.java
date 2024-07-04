aed26aa384229fe2d8cb892b5bac315f2e89b00d
gourisuraj <gouri.suraj@mail.utoronto.ca>
2024-07-04 18:01:15 -0400

    reconfigured Review class

--- src/entity/Review.java
+++ src/entity/Review.java
@@ -1,3 +1,21 @@
 package entity;
 
+public class Review {
 
+    private final int value;
+    private final String type;
+
+
+    Review(int value, String type) {
+        this.value = value;
+        this.type = type;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}
