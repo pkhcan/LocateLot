3caadd97e8b1b5bbed1339fd929806c0398873fe
gourisuraj <gouri.suraj@mail.utoronto.ca>
2024-07-08 17:02:19 -0400

    implemented better getReview methods (2)

---
+++ src/entity/FindingReview.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class FindingReview extends Review {
+
+    /**
+     * @param value
+     * @param type
+     */
+    FindingReview(int value, String type) {
+        super(value, type);
+    }
+}
