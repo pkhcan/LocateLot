28068a439c9e7f58ac13b66d4aaca1d41d91d030
Parmis Khaleghiyeh <parmis.khaleghiyeh@yahoo.com>
2024-07-10 22:05:47 -0400

    initial changes to DAO

--- src/data_access/GreenPDataAccessObject.java
+++ src/data_access/GreenPDataAccessObject.java
@@ -1,7 +1,19 @@
 package data_access;
 
-public class GreenPDataAccessObject {
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GreenPDataAccessObject {
+    public static void main(String[] args) throws IOException, ParseException {
+        JSONParser jsonparser = new JSONParser();
+        FileReader reader = new FileReader("/Users/parmis/CSC207/LocateLot/src/GreenPSourceData.json");
+        Object obj = jsonparser.parse(reader);
+
+    }
 
 }
 
