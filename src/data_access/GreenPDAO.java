e70d9087774fe34cb6060bf528c9f784d4434040
Nikoo Ghafouri <nikooghafouri@nikoos-air.home>
2024-07-11 14:18:15 -0400

    changes made include: replacing GreenPDataAccessObject with GreenPDAO interface, ParkingLotDAO added and partially implemented, ParkingLotFactory Added, Comments made to ParkingLot entity

---
+++ src/data_access/GreenPDAO.java
@@ -0,0 +1,26 @@
+package data_access;
+import entity.ParkingLot
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+// thinking of making this an abstract class extended by different DAOs
+
+public interface GreenPDAO {
+
+    private void parseFile() throws IOException, ParseException {
+
+    }
+
+    // List of all ParkingLot objects
+    ArrayList<ParkingLot> getParkingLots();
+
+    // Potentially filter here instead
+    ArrayList<ParkingLot> getParkingLotsPrice(double price);
+
+    ArrayList<ParkingLot> getParkingLotsRadius(float radius);
+    // How can we determine lat long of radius? Do we have live location?
+
+
+}
