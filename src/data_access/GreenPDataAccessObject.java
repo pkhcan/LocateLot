dc63f40b8cc052c11135d40cff105cc7e56b52e2
Nikoo Ghafouri <nikooghafouri@nikoos-air.home>
2024-07-10 23:06:32 -0400

    JSONArray obj created and plan outlined for GreenPDataAccessObject

--- src/data_access/GreenPDataAccessObject.java
+++ src/data_access/GreenPDataAccessObject.java
@@ -1,5 +1,7 @@
 package data_access;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -8,10 +10,48 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class GreenPDataAccessObject {
+    //
+
     public static void main(String[] args) throws IOException, ParseException {
         JSONParser jsonparser = new JSONParser();
-        FileReader reader = new FileReader("/Users/parmis/CSC207/LocateLot/src/GreenPSourceData.json");
+        FileReader reader = new FileReader("src/external_data/GreenPSourceData.json");
         Object obj = jsonparser.parse(reader);
+        JSONObject jsonObject = (JSONObject) obj;
+
+
+        // an array of all carparks/parkinglots within the JSON file
+        JSONArray jsonArray = (JSONArray) jsonObject.get("carparks");
+
+        /* Ideas of parsing
+         *
+         * GreenPDataAccessObject filterPriceData = GreenPDataAccessObject.MethodForPrice()
+         *
+         * our mini data = {"pricing": {1: number, 2: number}, "location": {1: [lat, long], 2: [lat, long]}}
+         * price data =
+         * lat long data =
+         *
+         * method does massive parsing = our mini data
+         *
+         * method for price ()
+         * for (iterate through ); {
+         *
+         *
+         * }
+         * method for lat long
+         *
+         * for () {
+         *
+         *
+         *
+         * }
+         *
+         * method for xyz
+         *
+         */
+
+
+
+
 
     }
 
