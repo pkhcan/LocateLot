e683e597328d5f9a832ed40396feec6bc8af2c36
Asma <asma.mohammed@mail.utoronto.ca>
2024-07-12 20:18:11 -0400

    Add closest parking lot logic (latitude and longtitude) and GeoApi

---
+++ src/data_access/GeoApiDAO.java
@@ -0,0 +1,18 @@
+package data_access;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+import java.io.IOException;
+
+public class GeoApiDAO {
+    private static final String API_KEY = "AIzaSyDeW__9cEG18wREriPhITvHPfdpMP4nQa8";
+    private static GeoApiContext CONTEXT = new GeoApiContext.Builder().apiKey(API_KEY).build();
+
+    public static GeocodingResult[] getLatitudeLongitude(String address) throws IOException, InterruptedException, ApiException {
+        return GeocodingApi.newRequest(CONTEXT).address(address).await();
+    }
+
+}
