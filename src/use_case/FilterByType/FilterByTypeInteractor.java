636e01b68d9827ed583bbf42f689a980079c4aae
Arnav Nagzirkar <arnav.nagzirkar@mail.utoronto.ca>
2024-07-22 00:40:51 -0400

    updated FilterByTypeInteractor

--- src/use_case/FilterByType/FilterByTypeInteractor.java
+++ src/use_case/FilterByType/FilterByTypeInteractor.java
@@ -1,8 +1,13 @@
 package use_case.FilterByType;
 
 public class FilterByTypeInteractor implements FilterByTypeInputBoundary {
+    final FilterByTypeData typeInputData;
 
-    public FilterByTypeInteractor() {}
+    public FilterByTypeInteractor(FilterByTypeData typeInputData) {
+        this.typeInputData = typeInputData;
+    }
 
-    public void filter(FilterByTypeData filterByTypeData) {}
+    public void filter(FilterByTypeData filterByTypeData) {
+
+    }
 }
