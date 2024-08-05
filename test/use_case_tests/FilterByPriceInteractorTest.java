85b835c0235742ca356b091c1c3618249f8411e8
pkhcan <parmis.khaleghiyeh@yahoo.com>
2024-08-05 19:49:20 -0400

    small change to getParkingLotPrice

---
+++ test/use_case_tests/FilterByPriceInteractorTest.java
@@ -0,0 +1,12 @@
+package use_case_tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterByPriceInteractorTest {
+
+    @Test
+    void execute() {
+    }
+}
